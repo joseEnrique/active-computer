ba8b279d10cc996fa024aa16640b578a6d9224a3
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-30 18:29:24 +0200

    add vagrant command

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+vagrant up
 gov test --readFromFile --path ./test10-elasticsearch.yaml
 gov test --readFromFile --path ./test50-elasticsearch.yaml
 gov test --readFromFile --path ./test100-elasticsearch.yaml
@@ -6,3 +7,4 @@ gov test --readFromFile --path ./test500-elasticsearch.yaml
 gov test --readFromFile --path ./test1000-elasticsearch.yaml
 gov test --readFromFile --path ./test5000-elasticsearch.yaml
 gov test --readFromFile --path ./test15000-elasticsearch.yaml
+vagrant destroy
