24d75cb0649ee518ec9472f3335705b77b563b7a
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-30 17:39:01 +0200

    chanegs in tests

--- test/test.sh
+++ test/test.sh
@@ -5,3 +5,4 @@ gov test --readFromFile --path ./test100-elasticsearch.yaml
 gov test --readFromFile --path ./test500-elasticsearch.yaml
 gov test --readFromFile --path ./test1000-elasticsearch.yaml
 gov test --readFromFile --path ./test5000-elasticsearch.yaml
+gov test --readFromFile --path ./test15000-elasticsearch.yaml
