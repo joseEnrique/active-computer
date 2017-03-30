2e07a482514747be15c1b3aa3fc34575d7215602
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-30 14:22:52 +0200

    remove node_modules

--- node_modules/governify-tester/src/prueba.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var prueba = require('./index.js');
-
-prueba.doParallelRequestFromfile('./configurations/config-exec.yaml').then(function(success) {
-
-    console.log(success);
-
-});
